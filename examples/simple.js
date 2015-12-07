b07e6f52fba501d170d1e7cb7b01281dd57797d5
ohalloranlab <damienmohalloran@yahoo.co.uk>
2015-12-06 20:15:15 -0500

    primermap 0.2.4

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,5 @@
 var primermap = require("primermap");
 
 var runBtn = document.getElementById("loader");
-var fileUpload = document.getElementById("fileInput");
 
-primermap(runBtn, fileUpload);
+primermap(runBtn);
